5876cdab8f2f4bfd2bc2099959ac919d5c18cebf
Divya Lakshmi <166861248+divyalakshmi0@users.noreply.github.com>
2025-01-05 20:02:32 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/BankApplication/config/SecurityConfig.java
+++ src/main/java/com/example/BankApplication/config/SecurityConfig.java
@@ -4,7 +4,6 @@ import com.example.BankApplication.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -43,18 +42,5 @@ public class SecurityConfig {
                         .permitAll())
                 .headers(header -> header.frameOptions(frameOptions -> frameOptions.sameOrigin()));
         return http.build();
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(accountService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
     }
 }
